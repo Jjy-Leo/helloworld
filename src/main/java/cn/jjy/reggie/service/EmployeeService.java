41fd9e9e47e7bc3cfdcf6cb297d05bee3261577f
jjy <jiajunyu2337@163.com>
2022-08-10 07:27:07 +0800

    测试

---
+++ src/main/java/cn/jjy/reggie/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package cn.jjy.reggie.service;
+
+import cn.jjy.reggie.entity.Employee;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface EmployeeService extends IService<Employee> {
+}
---
+++ src/main/java/cn/jjy/reggie/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,18 @@
+package cn.jjy.reggie.service.impl;
+
+import cn.jjy.reggie.entity.Employee;
+import cn.jjy.reggie.mapper.EmployeeMapper;
+import cn.jjy.reggie.service.EmployeeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: reggie_take_out
+ * @description:
+ * @author: jjy
+ * @create: 2022-08-10 07:21
+ **/
+@Service
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> {
+
+}
